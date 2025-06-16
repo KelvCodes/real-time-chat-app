cbe82aeb151025c37549e02e26e5f5194a3bc4af
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-16 15:17:12 -0700

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,7 @@
 
 };
-
-export const logout = (req, res) => {
-  try {
-    res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ message: "Logged out successfully" });
+s successfully" });
+  
   } catch (error) {
     console.log("Error in logout controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
