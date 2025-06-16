5dbdb2d50a1e12ffaca3d7a571a03fea4934b5dd
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-16 15:19:00 -0700

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,7 @@
 ByI
    
 
-export const checkAuth = (req, res) => {
-  try {
-    res.status(200).json(req.user);
+(req.user);
   } catch (error) {
     console.log("Error in checkAuth controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
