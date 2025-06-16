773533da18d07ef5d9b6d685890d7963904ff667
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-16 15:18:52 -0700

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,5 @@
 ByI
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    console.log("error in update profile:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+   
 
 export const checkAuth = (req, res) => {
   try {
