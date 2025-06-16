3c413605ce76a0f752746b3e53305c756ea7821b
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-16 15:06:47 -0700

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,5 @@
 
-     
-      res.status(400).json({ message: "Invalid user data" });
-    }
+
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
