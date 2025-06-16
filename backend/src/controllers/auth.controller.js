3b39c653406bd883bf7787fcbe9f04531a69bc65
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-16 14:53:37 -0700

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,4 @@
 
-
-    if (user) return res.status(400).json({ message: "Email already exists" });
-
-    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
