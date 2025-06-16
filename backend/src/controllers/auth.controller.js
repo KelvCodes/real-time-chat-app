1eef71181493658250bc0dd533dac92e1495516c
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-16 15:19:08 -0700

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,4 @@
 ByI
-   
-
-(req.user);
-  } catch (error) {
-    console.log("Error in checkAuth controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+  n({ message: "Internal Server Error" });
   }
 };
