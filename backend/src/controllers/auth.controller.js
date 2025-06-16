cf36bd5cc3e3eeaa4afe0c8fbd188f2a42ea4318
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-16 14:52:57 -0700

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,4 @@
 
-export const signup = async (req, res) => {
-  const { fullName, email, password } = req.body;
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
