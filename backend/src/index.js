addd88809bd37cff6c03381f32580cd716dccf60
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-17 13:42:01 -0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,1 @@
-// Import required dependencies
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import path from "path";
-import helmet from "helmet"; // Security headers
-import morgan from "morgan"; // Request logging
+
