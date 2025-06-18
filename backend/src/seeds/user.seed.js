d9993b0c18d3eb06972bbbbf1c4974431c4e3e8e
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-18 10:56:10 -0700

    Update user.seed.js

--- backend/src/seeds/user.seed.js
+++ backend/src/seeds/user.seed.js
@@ -1,9 +1,5 @@
 ame: "Charlotte Williams",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/women/7.jpg",
-  },
-  {
-    email: "amelia.garcia@example.com",
+amelia.garcia@example.com",
     fullName: "Amelia Garcia",
     password: "123456",
     profilePic: "https://randomuser.me/api/portraits/women/8.jpg",
