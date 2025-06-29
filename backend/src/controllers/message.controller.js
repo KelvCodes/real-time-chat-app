24d92995c8cef35d956495da4bc0d9a197931369
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-29 13:23:08 -0700

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,4 @@
 
-    res.status(200).json(messages);
-  } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
