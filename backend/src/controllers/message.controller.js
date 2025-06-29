47cfd92408d9439ee00f7333bff5fa10f94b54da
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-29 13:18:37 -0700

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,4 @@
 
-
-export const getUsersForSidebar = async (req, res) => {
-  try {
     const loggedInUserId = req.user._id;
     const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
