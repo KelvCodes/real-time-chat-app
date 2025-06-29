7f42a753e614108e00adad3d51651816c6f27dd7
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-29 13:18:50 -0700

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,4 @@
 
-    const loggedInUserId = req.user._id;
     const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
     res.status(200).json(filteredUsers);
