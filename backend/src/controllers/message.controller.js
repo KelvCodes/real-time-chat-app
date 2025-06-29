fa71ff0726f9b51ee861b642e71fc7ffdb5716af
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-29 13:29:54 -0700

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,4 @@
-
-    }
-
-    res.status(201).json(newMessage);
+tatus(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
