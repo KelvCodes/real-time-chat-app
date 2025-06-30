0c25b0f73c90166b382047531b58c97596ba8090
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-30 06:01:27 -0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,4 @@
 
-      cors({
-        origin: this.config.isProduction ? this.config.clientUrl : 'http://localhost:5173',
-        credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
       })
